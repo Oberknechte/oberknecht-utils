e97755681fb85db59c24f1879585b9c7059578f4
jubewe <ju.be.we@posteo.de>
2023-06-06 13:48:21 +0200

    v1.1.7

--- lib-js/utils/message.Emotes.js
+++ lib-js/utils/message.Emotes.js
@@ -5,9 +5,11 @@ function messageEmotes(rawEmotes) {
     if (!(rawEmotes ?? undefined))
         return {};
     let r = {};
-    rawEmotes?.split(",")?.map(a => {
+    rawEmotes?.split(",")?.forEach(a => {
         let b = a.split(":");
-        let c = b[1].split("-");
+        if (!b?.[0])
+            return;
+        let c = b?.[1]?.split("-");
         r[b[0]] = {
             "start": c[0],
             "end": c[1]
--- lib-ts/utils/message.Emotes.ts
+++ lib-ts/utils/message.Emotes.ts
@@ -8,9 +8,10 @@ export type messageEmotesReturn = Record<string, messageEmote>;
 export function messageEmotes(rawEmotes: string): messageEmotesReturn {
     if (!(rawEmotes ?? undefined)) return {};
     let r = {};
-    rawEmotes?.split(",")?.map(a => {
+    rawEmotes?.split(",")?.forEach(a => {
         let b = a.split(":");
-        let c = b[1].split("-");
+        if(!b?.[0]) return;
+        let c = b?.[1]?.split("-");
         r[b[0]] = {
             "start": c[0],
             "end": c[1]
