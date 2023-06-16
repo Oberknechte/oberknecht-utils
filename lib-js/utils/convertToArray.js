646f4bf8f5b835801e92c7b7f4922540a1e408b0
jubewe <ju.be.we@posteo.de>
2023-06-16 17:19:45 +0200

    v1.3.1

--- lib-js/utils/convertToArray.js
+++ lib-js/utils/convertToArray.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertToArray = void 0;
+const _1 = require(".");
 const recreate_1 = require("./recreate");
 // export function convertToArray(arr: Array<any> | any, returnundefined?: boolean | any) {
 function convertToArray(arr, returnundefined) {
     let arr_ = (0, recreate_1.recreate)(arr);
-    if (!(arr_ ?? undefined))
-        return (returnundefined ? undefined : []);
-    return (!Array.isArray(arr_) ? [arr_] : arr_);
+    if ((0, _1.isNullUndefined)(arr_))
+        return returnundefined ? undefined : [];
+    return !Array.isArray(arr_) ? [arr_] : arr_;
 }
 exports.convertToArray = convertToArray;
-;
--- lib-ts/utils/convertToArray.ts
+++ lib-ts/utils/convertToArray.ts
@@ -1,8 +1,12 @@
+import { isNullUndefined } from ".";
 import { recreate } from "./recreate";
 
 // export function convertToArray(arr: Array<any> | any, returnundefined?: boolean | any) {
-export function convertToArray<returnundefinedBoolean extends boolean>(arr: Array<any> | any, returnundefined?: returnundefinedBoolean | undefined): returnundefinedBoolean extends true ? Array<any> | undefined : Array<any> {
-    let arr_ = recreate(arr);
-    if (!(arr_ ?? undefined)) return (returnundefined ? undefined as any : []);
-    return (!Array.isArray(arr_) ? [arr_] : arr_);
-};
+export function convertToArray<returnundefinedBoolean extends boolean>(
+  arr: Array<any> | any,
+  returnundefined?: returnundefinedBoolean | undefined
+): returnundefinedBoolean extends true ? Array<any> | undefined : Array<any> {
+  let arr_ = recreate(arr);
+  if (isNullUndefined(arr_)) return returnundefined ? (undefined as any) : [];
+  return !Array.isArray(arr_) ? [arr_] : arr_;
+}
