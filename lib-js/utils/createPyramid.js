5b29e46325d033486522adcf40c7fdc5f6ca2d89
jubewe <ju.be.we@posteo.de>
2023-08-07 22:06:29 +0200

    v1.6.0

--- lib-js/utils/createPyramid.js
+++ lib-js/utils/createPyramid.js
@@ -4,7 +4,8 @@ exports.createPyramid = void 0;
 const _1 = require(".");
 function createPyramid(n_, s) {
     let r = "";
-    let n = (0, _1.isEvan)(n_) ? n_ + 1 : n_;
+    let n__ = n_ && _1.regex.numregex().test(n_.toString()) ? parseInt(n_.toString()) : n_;
+    let n = n__ ? ((0, _1.isEvan)(n__) ? n__ + 1 : n__) : 5;
     let half = (0, _1.getFullNumber)(n / 2);
     for (let i = 1; i < n + 1; i++) {
         if (i > 1)
--- lib-ts/utils/createPyramid.ts
+++ lib-ts/utils/createPyramid.ts
@@ -1,8 +1,10 @@
-import { getFullNumber, isEvan } from ".";
+import { getFullNumber, isEvan, regex } from ".";
 
 export function createPyramid(n_: number, s: string) {
   let r: string = "";
-  let n = isEvan(n_) ? n_ + 1 : n_;
+  let n__ =
+    n_ && regex.numregex().test(n_.toString()) ? parseInt(n_.toString()) : n_;
+  let n = n__ ? (isEvan(n__) ? n__ + 1 : n__) : 5;
   let half = getFullNumber(n / 2);
 
   for (let i = 1; i < n + 1; i++) {
