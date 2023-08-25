cbbd1319c71a95d2e37277c13aa8a12b46136bca
jubewe <ju.be.we@posteo.de>
2023-08-25 10:24:20 +0200

    v1.6.5

--- lib-ts/utils/arrayModifiers/splice.ts
+++ lib-ts/utils/arrayModifiers/splice.ts
@@ -1,7 +1,7 @@
-import { recreate } from "..";
+import { isNullUndefined, recreate } from "..";
 
 export function splice(arr: any[], index: number, deleteCount?: number): any[] {
-  if (!arr || !index || !arr[index]) return arr;
+  if (!arr || isNullUndefined(index) || !arr[index]) return arr;
   let arr_ = recreate(arr);
   arr_.splice(index, deleteCount ?? 1);
   return arr_;
