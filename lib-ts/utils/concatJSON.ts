a0f51bf0087aaa0ca8b59da56aaf28660899b862
jubewe <ju.be.we@posteo.de>
2023-08-31 10:25:18 +0200

    v1.6.9

--- lib-ts/utils/concatJSON.ts
+++ lib-ts/utils/concatJSON.ts
@@ -1,9 +1,22 @@
+import { addKeysToObject, convertToArray, extendedTypeof } from ".";
+
 export function concatJSON(arr: Record<string, any>[]) {
+  let arr_ = convertToArray(arr);
   let r = {};
-  arr.forEach((a) => {
-    Object.keys(a).forEach((b) => {
-      r[b] = a[b];
-    });
+  arr_.forEach((a) => {
+    function appendObj(obj, obj2) {
+      Object.keys(obj2).forEach((b) => {
+        if (
+          obj[b] &&
+          extendedTypeof(obj[b]) === "json" &&
+          extendedTypeof(obj2[b]) === "json"
+        )
+          return appendObj(obj[b], obj2[b]);
+        obj[b] = obj2[b];
+      });
+    }
+
+    appendObj(r, a);
   });
 
   return r;
